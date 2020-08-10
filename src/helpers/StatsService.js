6fb361b2c7e433c055bd31241a5c2dbfebc94800
peaky76 <robertjamespeacock@gmail.com>
2020-08-10 22:01:26 +0100

    API documented as comments in StatsService

--- src/helpers/StatsService.js
+++ src/helpers/StatsService.js
@@ -1,8 +1,28 @@
 const baseURL = "http://api.coronavirus.data.gov.uk/v1/data?";
 
+// API Issues:
+// Call using newCasesByPublishDate, newCasesBySpecimenDate, newAdmissions,
+// newDeathsByPublishDate, newDeathsByDeathDate
+
+// Nations give all stats, except:
+//      Scotland doesn't give deaths by death date
+//      N Ireland doesn't give deaths by death date or cases by specimen date
+
+// Regions only give cases by specimen date, deaths by death date, no admissions
+
+// UTLAs only give cases, not deaths or admissions
+//      Scottish UTLAs only give stats for yesterday
+//      N Ireland doesn't have any UTLAs
+
+//filters = areaType, areaName, date
+//e.g. filters=areaType=nation;areaName=england;date=2020-08-01
+
+//structure =
+//e.g. &structure=
+
 export default {
   getData(structureString) {
-    let filterString = "areaType=utla;areaName=City of Edinburgh";
+    let filterString = "areaType=utla;areaName=Bridgend";
     // let structureString = {};
     const search =
       baseURL +
@@ -15,34 +35,6 @@ export default {
     return fetch(search).then((res) => res.json());
   },
 };
-
-//filters = areaType, areaName, date
-//e.g. filters=areaType=nation;areaName=england;date=2020-08-01
-
-//structure =
-//e.g. &structure=
-
-// structure elements and availablity:
-// "newCasesByPublishDate", NATION, UTLA
-// "cumCasesByPublishDate", NATION, UTLA
-// "newCasesBySpecimenDate", NATION, REGION, UTLA
-// "cumCasesBySpecimenDate", NATION, REGION, UTLA
-// "maleCases", NATION (BY AGE)
-// "femaleCases", NATION (BY AGE)
-// "newAdmissions", NATION
-// "cumAdmissions", NATION
-// "cumAdmissionsByAge", NATION
-// "cumTestsByPublishDate", NATION
-// "newTestsByPublishDate", NATION
-// "covidOccupiedMVBeds", NATION
-// "hospitalCases", NATION
-// "plannedCapacityByPublishDate", NATION
-// "newDeathsByPublishDate", NATION, UTLA
-// "cumDeathsByPublishDate", NATION
-// "newDeathsByDeathDate", NATION, REGION
-// "cumDeathsByDeathDate", NATION, REGION
-// "femaleDeaths", ??
-// "maleDeaths", ??
 
 // THIS API CALL WORKS FOR NATIONS
 // https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=Scotland&structure={%22areaType%22:%22areaType%22,%22code%22:%22areaCode%22,%22areaName%22:%22areaName%22,%22date%22:%22date%22,%22newCases%22:%22newCasesByPublishDate%22,%22newAdmissions%22:%22newAdmissions%22,%22newDeaths%22:%22newDeathsByPublishDate%22}
