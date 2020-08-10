8f73f36855bd365a3a9b054c9d867801e10e2240
peaky76 <robertjamespeacock@gmail.com>
2020-08-10 11:15:18 +0100

    buildFilters fn to create filter objects

--- src/components/input_sub/InputCheckbox.vue
+++ src/components/input_sub/InputCheckbox.vue
@@ -1,15 +1,26 @@
 <template>
-  <input type="checkbox" />
+  <div>
+    <input type="checkbox" />
+    <div v-for="(filter, index) in filters" :key="index">{{filter}}</div>
+  </div>
 </template>
 
 <script>
-import { eventBus } from '@/main';
+import { eventBus } from "@/main";
+import FilterBuilder from "@/helpers/FilterBuilder.js";
 
 export default {
   name: "input-checkbox",
+  data() {
+    return {
+      filters: [],
+    };
+  },
+  mounted() {
+    this.filters = FilterBuilder.buildFilters();
+  },
 };
 </script>
 
 <style>
-
 </style>
