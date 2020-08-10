c548b0970053e71b06918fbcd199d4f0a7b0b9ad
peaky76 <robertjamespeacock@gmail.com>
2020-08-10 22:15:06 +0100

    accepted merge

--- src/helpers/StatsService.js
+++ src/helpers/StatsService.js
@@ -21,35 +21,40 @@ const baseURL = "http://api.coronavirus.data.gov.uk/v1/data?";
 //e.g. &structure=
 
 export default {
-<<<<<<< HEAD
-  getData(structureString) {
-    let filterString = "areaType=utla;areaName=Bridgend";
-    // let structureString = {};
+  //   getData(structureString) {
+  //     let filterString = "areaType=utla;areaName=Bridgend";
+  //     // let structureString = {};
+  //     const search =
+  //       baseURL +
+  //       "filters=" +
+  //       filterString +
+  //       '&structure={"date":"date",' +
+  //       structureString +
+  //       "}";
+  //     console.log(search);
+  //     return fetch(search).then((res) => res.json());
+  //   },
+  // };
+
+  // THIS API CALL WORKS FOR NATIONS
+  // https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=Scotland&structure={%22areaType%22:%22areaType%22,%22code%22:%22areaCode%22,%22areaName%22:%22areaName%22,%22date%22:%22date%22,%22newCases%22:%22newCasesByPublishDate%22,%22newAdmissions%22:%22newAdmissions%22,%22newDeaths%22:%22newDeathsByPublishDate%22}
+
+  // THIS API CALL WORKS FOR REGIONS
+  // https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=region;areaName=North%20East;date=2020-05-15&structure={%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22date%22:%22date%22,%22newCases%22:%22newCasesBySpecimenDate%22,%22cumulativeCases%22:%22cumCasesBySpecimenDate%22,%22newDeaths%22:%22newDeathsByDeathDate%22,%22cumulativeDeaths%22:%22cumDeathsByDeathDate%22}
+  getData(location, filters) {
+    console.log("statService location:", location);
+    console.log("statService filters:", filters);
     const search =
       baseURL +
-      "filters=" +
-      filterString +
+      "filters=areaName=" +
+      location.areaName +
+      ";" +
+      "areaType=" +
+      location.areaType +
       '&structure={"date":"date",' +
-      structureString +
+      filters +
       "}";
     console.log(search);
     return fetch(search).then((res) => res.json());
   },
 };
-
-// THIS API CALL WORKS FOR NATIONS
-// https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=Scotland&structure={%22areaType%22:%22areaType%22,%22code%22:%22areaCode%22,%22areaName%22:%22areaName%22,%22date%22:%22date%22,%22newCases%22:%22newCasesByPublishDate%22,%22newAdmissions%22:%22newAdmissions%22,%22newDeaths%22:%22newDeathsByPublishDate%22}
-
-// THIS API CALL WORKS FOR REGIONS
-// https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=region;areaName=North%20East;date=2020-05-15&structure={%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22date%22:%22date%22,%22newCases%22:%22newCasesBySpecimenDate%22,%22cumulativeCases%22:%22cumCasesBySpecimenDate%22,%22newDeaths%22:%22newDeathsByDeathDate%22,%22cumulativeDeaths%22:%22cumDeathsByDeathDate%22}
-=======
-    getData(location, filters) {
-        console.log('statService location:', location)
-        console.log('statService filters:', filters)
-        const search = (baseURL + 'filters=areaName=' + location.areaName + ';' + 'areaType=' + location.areaType + '&structure={"date":"date",' + filters + '}')
-        console.log(search)
-        return fetch(search)
-        .then(res => res.json())
-    }
-}
->>>>>>> develop
