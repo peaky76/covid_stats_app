8b69b771ea776eea603674cdf032a16dc2cc7ae7
Rebeka Geczi <geczirebeka@gmail.com>
2020-08-10 17:17:11 +0100

    Stored dates in vue

--- src/components/input_sub/InputDateSlider.vue
+++ src/components/input_sub/InputDateSlider.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>DATE SLIDER HERE</div>
+  <div>
+    <input v-model="startDate" type="date" id="start-date">
+    <input v-model="endDate" type="date" id="end-date">
+    DATE SLIDER HERE</div>
 </template>
 
 <script>
 import { eventBus } from '@/main';
+import moment from 'moment';
 
 export default {
   name: "input-date-slider",
+  data() {
+    return {
+      startDate: '',
+      endDate: '',
+    }
+  },
+  methods: {
+  }
 };
 </script>
 
 <style>
-</style>
+
+
+
+/* let date = moment.utc(date_str); */
