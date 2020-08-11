634879e9b9abd984af57249688cf2fdae9e13ebf
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-11 23:10:32 +0100

    got place name to show when result comes in

--- src/components/result_sub/ResultDetail.vue
+++ src/components/result_sub/ResultDetail.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h3 v-if="result">{{result[0].areaName}}</h3>
+    <h4>{{this.dateGetter}}</h4> -->
     <p v-for="(item, index) in result" :key="index">{{item}}</p>
   </div>
 </template>
@@ -8,6 +10,30 @@
 export default {
   name: "result-detail",
   props: ["result"],
+  data()  {
+    return {
+      startDate: '',
+      endDate: ''
+    }
+  },
+  computed: {
+    dateGetter() {
+      if (this.results) {
+        this.startDate = result[0].date
+        this.endDate = result[result.length - 1].date
+        return `Range of Dates: ${startDate} to ${endDate}`
+      } else {
+        return 'No date(s) selected';
+      }
+    },
+    getAreaName() {
+      if (this.results) {
+        return result[0].areaName
+      } else {
+        return 'No location selected'
+      }
+    }
+  }
 };
 </script>
 
