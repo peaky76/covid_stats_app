47f76a3bdbb6d65d57a6dec380998c3323fd1f0a
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-12 00:32:38 +0100

    selected filters now appear in result detail

--- src/components/result_sub/ResultDetail.vue
+++ src/components/result_sub/ResultDetail.vue
@@ -2,7 +2,7 @@
   <div>
     <h3 v-if="result">{{result[0].areaName}}</h3>
     <h4 v-if="result">Range of Dates: {{result[0].date}} to {{result[result.length - 1].date}}</h4>
-    <p v-for="(item, index) in result" :key="index">{{item}}</p>
+    <p v-for="(item, index) in result" :key="index">Date: {{item.date}}</p>
   </div>
 </template>
 
@@ -10,39 +10,74 @@
 export default {
   name: "result-detail",
   props: ["result"],
-  data()  {
-    return {
-      startDate: '',
-      endDate: ''
-    }
-  },
+  // data()  {
+  //   return {
+  //     startDate: '',
+  //     endDate: ''
+  //   }
+  // },
   computed: {
-    firstDateGetter() {
-      if (this.results) {
-        this.startDate = result[0].date
-        // this.endDate = result[result.length - 1].date
-        return this.startDate
-      } else {
-        return 'No start date selected';
+    headings() {
+      if (this.result) {
+        return Object.keys(this.result[0]);
       }
     },
-    lastDateGetter() {
-      if (this.results) {
-        // this.startDate = result[0].date
-        this.endDate = result[result.length - 1].date
-        return this.endDate
-      } else {
-        return 'No end date selected';
+    chartData() {
+      let dataToDisplay = [];
+      if (this.result) {
+        const newHeading = this.headings.splice(0, 2)
+        dataToDisplay.push(this.headings);
+        this.dataRows.forEach((dataRow) => {
+          dataToDisplay.push(dataRow);
+        });
+      }
+      return dataToDisplay;
+    },
+    headings() {
+      if (this.result) {
+        return Object.keys(this.result[0]);
       }
     },
-    getAreaName() {
-      if (this.results) {
-        return result[0].areaName
-      } else {
-        return 'No location selected'
+    dataRows() {
+      if (this.result) {
+        let dataRows = [];
+        this.result.forEach((item) => {
+          let dataRow = [];
+          this.headings.forEach((heading) => {
+            dataRow.push(item[heading]);
+          });
+          dataRows.push(dataRow);
+        });
+        return dataRows;
       }
-    }
+    },
   }
+  //   firstDateGetter() {
+  //     if (this.results) {
+  //       this.startDate = result[0].date
+  //       // this.endDate = result[result.length - 1].date
+  //       return this.startDate
+  //     } else {
+  //       return 'No start date selected';
+  //     }
+  //   },
+  //   lastDateGetter() {
+  //     if (this.results) {
+  //       // this.startDate = result[0].date
+  //       this.endDate = result[result.length - 1].date
+  //       return this.endDate
+  //     } else {
+  //       return 'No end date selected';
+  //     }
+  //   },
+  //   getAreaName() {
+  //     if (this.results) {
+  //       return result[0].areaName
+  //     } else {
+  //       return 'No location selected'
+  //     }
+  //   }
+  // }
 };
 </script>
 
