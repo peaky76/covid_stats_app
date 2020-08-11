ac7c01a575f23b8a57919b39dd59944d5bafd9d7
Rebeka Geczi <geczirebeka@gmail.com>
2020-08-11 14:21:16 +0100

    removed refresh button

--- src/components/input_sub/InputDateSlider.vue
+++ src/components/input_sub/InputDateSlider.vue
@@ -1,53 +1,46 @@
 <template>
   <div>
-    <input v-model="startDate" type="date" id="start-date" v-on:change="dateRange">
-    <input v-model="endDate" type="date" id="end-date" v-on:change="dateRange">
-    <button >Duration </button>
+    <input v-model="startDate" type="date" id="start-date" v-on:change="dateRange" />
+    <input v-model="endDate" type="date" id="end-date" v-on:change="dateRange" />
   </div>
 </template>
 
 <script>
-import { eventBus } from '@/main';
-import moment from 'moment';
+import { eventBus } from "@/main";
+import moment from "moment";
 
 export default {
   name: "input-date-slider",
   data() {
     return {
-      startDate: '',
-      endDate: ''
-    }
+      startDate: "",
+      endDate: "",
+    };
   },
   computed: {
     dateRange() {
-      const startMoment = moment(this.startDate)
-      const endMoment = moment(this.endDate)
-      let allDates = []
-      for (let dates = startMoment; dates <= endMoment; dates.add(1, 'days')) {
+      const startMoment = moment(this.startDate);
+      const endMoment = moment(this.endDate);
+      let allDates = [];
+      for (let dates = startMoment; dates <= endMoment; dates.add(1, "days")) {
         allDates.push(dates.format("YYYY-MM-DD"));
       }
-    return allDates;
+      return allDates;
     },
   },
-  mounted() {
-    
-  },
+  mounted() {},
   methods: {
     durationConverter() {
-      eventBus.$emit("dates", this.dateRanges);
-    }
+      eventBus.$emit("dates", this.dateRange);
+    },
     // let dates = startMoment.add(1, 'days')
     // console.log('dates', dates.format("YYYY-MM-DD"))
     // this.duration = moment.duration(endMoment.diff(startMoment, 'days'))
     // console.log('duration:', this.duration)
     // console.log(this.duration().format())
-    
-  }
+  },
 };
 </script>
 
 <style>
-
-
-
 /* let date = moment.utc(date_str); */
